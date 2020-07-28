f96bbbdbabd250d37987704ed2456233cee83c42
Ana <anarps_@hotmail.com>
2020-07-28 19:51:29 -0300

    Setup Inicial do Projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Menu from './components/Menu'
+//import dadosIniciais from '.data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
 
 function App() {
   return (
     <div >
-      Hello World
+      <Menu />
+      
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+import Button from '../Button/index'
+//import ButtonLink from '../components/ButtonLink/index'
 
 function Menu() {
+    return (
+    <nav className="Menu">
+        <a href="/">
+            <img className="Logo" src={Logo} alt="Aluraflixlogo"/>
+        </a>
+
+        <Button as ="a" href="/" className ="ButtonLink">
+            Novo vídeo
+        </Button>
+</nav>
+    )
 }
 
 export default Menu
+
