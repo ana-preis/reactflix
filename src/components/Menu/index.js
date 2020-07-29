1e8be8e2c3ae9cfdea1563ecd26c75d994984f8d
Ana <anarps_@hotmail.com>
2020-07-29 17:57:52 -0300

    Commit rapidao aqui

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 import Button from '../Button/index'
@@ -7,15 +8,15 @@ import Button from '../Button/index'
 function Menu() {
     return (
     <nav className="Menu">
-        <a href="/">
-            <img className="Logo" src={Logo} alt="Aluraflixlogo"/>
-        </a>
+        <Link to="/">
+            <img className="Logo" src={Logo} alt="Aluraflix logo"/>
+        </Link>
 
-        <Button as ="a" href="/" className ="ButtonLink">
+        <Button as={Link} to="/cadastro/video" className ="ButtonLink">
             Novo vídeo
         </Button>
-</nav>
-    )
+    </nav>
+    );
 }
 
 export default Menu
