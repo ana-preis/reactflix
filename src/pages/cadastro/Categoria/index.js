c3c03aed149ef7b15ba2e4f5d3bff555b05243d3
Ana <anarps_@hotmail.com>
2020-08-03 19:50:52 -0300

    metade da aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault'
 
 function CadastroCategoria() {
-    return (
-      <PageDefault>
-        <h1>Cadastro de Categoria </h1>
+  const [categorias, setCategorias] = useState(['Teste']);
+  
+  const valoresIniciais = {
+    nome:'categoria inicial',
+    descricao:'descrição',
+    cor:'#bbb',
+  }
 
-        <form>
+  const [values, setValues] = useState(valoresIniciais);
+  
+  //chave: nome, descricao, blabla
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor, //nome: 'valor'
+    })
+  }
+  
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome} </h1>
 
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-            />
-          </label>
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values
+        ])
+      }}>
 
-          <button>
-            Cadastrar
-          </button>
-        </form>
+        <div>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+            value= {values.nome}
+            onChange={function funcaoHandlerQueOErroPeddiu(infosDoEvento) {
+              setValue('nome', infosDoEvento.target.value)
+            }}
+          />
+        </label>
+        </div>
+
+        <div>
+        <label>
+          Descrição:
+          <textarea
+            type="text"
+            value= {values.descricao}
+            onChange={function funcaoHandlerQueOErroPeddiu(infosDoEvento) {
+              //setValues(infosDoEvento.target.value)
+            }}
+          />
+        </label>
+        </div>
+
+        <div>
+        <label>
+          Cor:
+          <input
+            type="color"
+            value= {values.cor}
+            onChange={function funcaoHandlerQueOErroPeddiu(infosDoEvento) {
+              setValues(infosDoEvento.target.value)
+            }}
+          />
+        </label>
+        </div>
+        
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
         <Link to='/'>
             Ir para Home
